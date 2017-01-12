17b1b44cc5f8bfe336b9d27b655c747c33cccef2
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-01-11 20:17:02 -0800

    test report show

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -82,7 +82,10 @@ class DocumentsController extends Controller
 
     public function showReport(Test $test, Document $document)
     {
-      return view('tests.reports.show', compact('test', 'document'));
+      // return view('tests.reports.show', compact('test', 'document'))
+      return response("https://s3-us-west-2.amazonaws.com/ssiwebstorage" . $document->path . '/' . $document->file_name . '.' . $document->ext)
+        ->header('Content-Type', 'application/pdf')
+        ->header('Content-Disposition', 'inline');
     }
 
     public function edit($id)
