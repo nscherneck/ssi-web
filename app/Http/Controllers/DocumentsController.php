7346840ae6329507c0a69981c9c7abc7f09830ec
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-01-11 22:57:31 -0800

    test report show

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\File;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Response;
 
 use App\Traits\ManagesReports;
