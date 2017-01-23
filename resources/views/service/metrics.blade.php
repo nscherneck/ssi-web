a51d3537d220c2b2e96c2713c0b034ae22776a26
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-01-22 18:14:20 -0800

    metrics

--- resources/views/service/metrics.blade.php
+++ resources/views/service/metrics.blade.php
@@ -4,8 +4,8 @@
 
 @section('head')
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js"></script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js"></script>
   <script type="text/javascript">
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
@@ -44,6 +44,7 @@
       chart.draw(data, options);
 
     }
+
   </script>
 
 @stop
@@ -60,22 +61,131 @@
 
 <div class="row">
 
-  <div class="col-lg-9">
+  <div class="col-lg-4">
 
-    <!-- Google Visualization donut chart -->
-
-    <div class="panel panel-default">
-    <div class="panel-heading">Total Systems</div>
+    <div class="panel panel-default panel-info">
+    <div class="panel-heading"><i class="fa fa-bar-chart" aria-hidden="true"></i> Total Systems by Type ({{ $quantityTotal }})</div>
       <div class="panel-body">
-
-        <div id="donutchart" style="width: 800px; height: 380px;"></div>
-
+        <div>
+          <canvas id="myDonutChart" width="500" height="300"></canvas>
+        </div>
       </div>
     </div>
 
-    <!-- end -->
+  </div>
 
-    <canvas id="myBarChart" height="300"></canvas>
+  <div class="col-lg-4">
+
+  <script type="text/javascript">
+
+  var ctx1 = document.getElementById("myDonutChart");
+
+  Chart.defaults.global.legend.display = false;
+
+  var donutdata = {
+    labels: [
+      "Clean Agent",
+      "Fire Alarm",
+      "Inert Gas",
+      "Dry Chemical",
+      "Wet Chemical",
+      "Aerosol",
+      "Fire Sprinkler (wet)",
+      "Fire Sprinkler (dry)",
+      "Fire Sprinkler (preaction)",
+      "Fire Sprinkler (deluge)",
+      "Fire Sprinkler (foam)",
+      "Fire Extinguisher",
+      "Low-Pressure CO2",
+      "High-Pressure CO2",
+      "Air Sampling",
+      "High-Expansion Foam",
+      "Watermist",
+      "Backflow Preventer"
+    ],
+  datasets: [
+      {
+          data: [
+            {{ $quantitySystemType[0] }},
+            {{ $quantitySystemType[1] }},
+            {{ $quantitySystemType[2] }},
+            {{ $quantitySystemType[3] }},
+            {{ $quantitySystemType[4] }},
+            {{ $quantitySystemType[5] }},
+            {{ $quantitySystemType[6] }},
+            {{ $quantitySystemType[7] }},
+            {{ $quantitySystemType[8] }},
+            {{ $quantitySystemType[9] }},
+            {{ $quantitySystemType[10] }},
+            {{ $quantitySystemType[11] }},
+            {{ $quantitySystemType[12] }},
+            {{ $quantitySystemType[13] }},
+            {{ $quantitySystemType[14] }},
+            {{ $quantitySystemType[15] }},
+            {{ $quantitySystemType[16] }},
+            {{ $quantitySystemType[17] }}
+          ],
+          backgroundColor: [
+              "#00bfff",
+              "#ff0000",
+              "#00ff00",
+              "#0040ff",
+              "#8000ff",
+              "#ff0080",
+              "#ff8000",
+              "#00ff80",
+              "#ffff00",
+              "#00ffbf",
+              "#cc3333",
+              "#ffcccc",
+              "#ffe6e6",
+              "#bfff00",
+              "#8000ff",
+              "#ff00ff",
+              "#bfff00",
+              "#0080ff"
+          ],
+          hoverBackgroundColor: [
+              "#00bfff",
+              "#ff0000",
+              "#00ff00",
+              "#0040ff",
+              "#8000ff",
+              "#ff0080",
+              "#ff8000",
+              "#00ff80",
+              "#ffff00",
+              "#00ffbf",
+              "#cc3333",
+              "#ffcccc",
+              "#ffe6e6",
+              "#bfff00",
+              "#8000ff",
+              "#ff00ff",
+              "#bfff00",
+              "#0080ff"
+          ]
+      }]
+  };
+
+  var myDonutChart = new Chart(ctx1, {
+    type: 'doughnut',
+    data: donutdata,
+    options: {
+        responsive: true,
+      }
+  });
+
+  </script>
+
+  <div class="panel panel-default panel-info">
+  <div class="panel-heading"><i class="fa fa-bar-chart" aria-hidden="true"></i> Tests per Month</div>
+    <div class="panel-body">
+
+    <canvas id="myBarChart" width="250" height="150"></canvas>
+
+  </div>
+</div>
 
     <script type="text/javascript">
       var ctx = document.getElementById("myBarChart");
