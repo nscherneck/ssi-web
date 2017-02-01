a2ff797d1fd8444edef87ed49224fd25aa5e62cc
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-02-01 14:32:55 -0800

    config edit

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Codecourse\Notify\NotifyServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,7 +228,6 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'Notify' => Codecourse\Notify\Facades\Notify::class,
 
     ],
 
