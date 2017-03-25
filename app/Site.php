8c94725355da2f064bcbef31bdd977527ad3fe69
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-03-24 17:52:56 -0700

    date fix for sites.show

--- app/Site.php
+++ app/Site.php
@@ -71,6 +71,18 @@ class Site extends Model
         return $this->belongsTo('App\User', 'updated_by');
     }
 
+    public function getFormattedUpdatedAtAttribute()
+    {
+        return $this->updated_at->setTimezone('America/Los_Angeles')
+            ->format('F j, Y, g:i a');
+    }
+
+    public function getFormattedCreatedAtAttribute()
+    {
+        return $this->created_at->setTimezone('America/Los_Angeles')
+            ->format('F j, Y, g:i a');
+    }
+
     public function count_systems($id) 
     {
         $systems_quantity = DB::table('systems')
