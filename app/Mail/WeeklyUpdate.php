f3a5e71ef938a17b9e4bda32782d6fcf041c6155
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-04-02 10:42:42 -0700

    update weeklyupdate email

--- app/Mail/WeeklyUpdate.php
+++ app/Mail/WeeklyUpdate.php
@@ -31,8 +31,46 @@ class WeeklyUpdate extends Mailable
         $address = 'nathan@suppression.com';
         $name = 'Nathan Scherneck';
         $subject = 'Weekly Update from SSI-Extranet';
+        
+        $end_date_raw = Carbon::now('America/Los_Angeles');
+        $end_date = $end_date_raw->format('Y-m-d');
+        $start_date = $end_date_raw->subWeeks(1)->format('Y-m-d');
+        
+        $start_date_testing_raw = Carbon::now('America/Los_Angeles');
+        $start_date_testing = $start_date_testing_raw->format('Y-m-d');
+        $end_date_testing = $start_date_testing_raw
+            ->addMonths(3)
+            ->format('Y-m-d');
+        
+        $newcustomers = Customer::orderBy('created_at', 'desc')
+            ->whereBetween('created_at', [$start_date, $end_date])
+            ->get();
+        
+        $newsites = Site::orderBy('created_at', 'desc')
+            ->whereBetween('created_at', [$start_date, $end_date])
+            ->get();
+        
+        $newsystems = System::orderBy('created_at', 'desc')
+            ->whereBetween('created_at', [$start_date, $end_date])
+            ->get();
+        
+        $newtests = Test::orderBy('test_date', 'desc')
+            ->whereBetween('test_date', [$start_date, $end_date])
+            ->get();
     
-        return $this->view('email.weeklyupdate')    
+        $systemsduefortest = System::orderBy('next_test_date', 'asc')
+            ->where('next_test_date', '!=', null)    
+            ->whereBetween('next_test_date', [$start_date_testing, $end_date_testing])    
+            ->get();    
+    
+        return $this->view('email.weeklyupdate', compact(
+            'newcustomers', 
+            'newsites', 
+            'newsystems', 
+            'newtests', 
+            'systemsduefortest'
+                )
+            )    
             ->from($address, $name)    
             ->subject($subject);
     }
