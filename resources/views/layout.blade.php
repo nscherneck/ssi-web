5d6e79f4b1ef34ed8dbcf3fc9ac7b9cb33b8ebcf
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-04-24 14:45:38 -0700

    add apple touch favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,6 +13,7 @@
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="shortcut icon" href="{{{ asset('images/favicon-32x32.png') }}}">
+    <link rel="apple-touch-icon" href="{{{ asset('images/favicon-57x57.png') }}}"/>
     <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
     <link rel="stylesheet" href="{{ asset('css/sweetalert.css') }}">
 
