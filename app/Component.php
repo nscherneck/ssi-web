ce9a601916575dc3f3f82ff16ea42e00c528a288
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-10-01 23:36:37 -0700

    styleci fixes

--- app/Component.php
+++ app/Component.php
@@ -7,15 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Component extends Model
 {
     /**
-    * The attributes that should be hidden for arrays.
-    *
-    * @var array
-    */
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = ['created_at', 'updated_at'];
 
     /**
-    * Get the Systems that this Component is associated with
-    */
+     * Get the Systems that this Component is associated with.
+     *
+     * @return  \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function systems()
     {
         return $this->belongsToMany('App\System', 'components_systems', 'component_id', 'system_id')
@@ -23,32 +25,38 @@ class Component extends Model
     }
 
     /**
-    * Get the Categories that this Component belongs to
-    */
+     * Get the Categories that this Component belongs to.
+     *
+     * @return  \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function componentCategory()
     {
         return $this->belongsTo('App\ComponentCategory');
     }
 
     /**
-    * Get the Manufacturer that this Component belongs to
-    */
+     * Get the Manufacturer that this Component belongs to.
+     *
+     * @return  \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function manufacturer()
     {
         return $this->belongsTo('App\Manufacturer')->orderBy('name', 'asc');
     }
 
     /**
-    * Get the Documents associated with this Component
-    */
+     * Get the Documents associated with this Component.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
     public function documents()
     {
         return $this->morphMany('App\Document', 'documentable');
     }
 
     /**
-    * Get the formatted description excerpt (100 characters in length) for this Component
-    */
+     * Get the formatted description excerpt (100 characters in length) for this Component.
+     */
     public function getFormattedDescriptionAttribute()
     {
         if (strlen($this->description) > 100) {
