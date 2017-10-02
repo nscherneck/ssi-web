efb42d8a72b5ab199caec0b5930c2c9725a560a9
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-10-01 23:20:13 -0700

    styleci fixes

--- app/BranchOffice.php
+++ app/BranchOffice.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class BranchOffice extends Model
 {
     /**
-    * @var bool False turns off timestamps for the model, delete to keep them on.
-    */
+     * @var bool False turns off timestamps for the model, delete to keep them on.
+     */
     public $timestamps = false;
 }
