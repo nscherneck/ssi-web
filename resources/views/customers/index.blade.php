10cff67ceffd74ed60cbaca8f7aa5e6b4052ba71
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-10-15 11:06:23 -0700

    change customers.index

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -14,49 +14,26 @@
     <h1>Customers <small>{{ $customers->count() }}</small></h1>
   </div>
   <hr>
-  @foreach($customers->chunk(3) as $chunk)
+  @foreach($customers->chunk(4) as $chunk)
   <div class="row">
     @foreach($chunk as $customer)
-      <div class="col-lg-4">
+      <div class="col-lg-3">
         <div class="panel panel-info">
           <div class="panel-heading">
             <h3 class="panel-title text-center">
               <a href="{{ $customer->path() }}">{{ $customer->name }}</a>
             </h3>
-          </div>
+          </div> <!-- ./panel-heading -->
           <div class="panel-body text-center">
             <strong>Sites: </strong><small>{{ $customer->sites_count }}</small> |
             <strong>Systems: </strong><small>{{ $customer->systems_count }}</small>
-          </div>
-        </div>
-      </div>
+          </div> <!-- ./panel-body -->
+        </div> <!-- ./panel -->
+      </div> <!-- ./column -->
     @endforeach
-  </div>
+  </div> <!-- ./row -->
   @endforeach
 
-{{--   <div class="titleBar" style="margin-top: 0">
-      <p>Customers ({{ $customers->count() }})</p>
-  </div>
-
-  <table class="table table-condensed">
-    <thead>
-      <tr>
-        <th><small>Customer</small></th>
-        <th><small>Sites</small></th>
-        <th><small>Systems</small></th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($customers as $customer)
-        <tr>
-        <td><small><a href="{{ $customer->path() }}">{{ $customer->name }}</a></small></td>
-        <td><small>{{ $customer->sites_count }}</small></td>
-        <td><small>{{ $customer->systems_count }}</small></td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table> --}}
-
-</div>
+</div> <!-- ./container -->
 
 @stop
