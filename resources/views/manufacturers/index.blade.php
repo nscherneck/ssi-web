446d7cfd9808232495aa4b1ece2ef0bf168b9a2c
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-10-17 04:29:12 -0700

    change manufacturers/index

--- resources/views/manufacturers/index.blade.php
+++ resources/views/manufacturers/index.blade.php
@@ -38,53 +38,6 @@
     @endforeach
   </div>
 
-  <div class="row">
-
-    <div class="col-lg-4 col-lg-offset-4">
-
-        <div class="panel panel-default" style="margin-top: 15px">
-
-    <div class="panel-heading text-center">
-      Manufacturers
-    </div>
-
-    <table class="table">
-      <thead>
-        <tr>
-          <th><small>Manufacturer</small></th>
-          <th class="text-center"><small>Components</small></th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($manufacturers as $manufacturer)
-          <tr>
-
-            <td width="70%">
-            <small>
-            <a href="/manufacturer/{{ $manufacturer->id }}">
-            {{ $manufacturer->name }}
-            </a>
-            </small>
-            </td>
-
-            <td width="30%" class="text-center">
-            <small>
-            {{ $manufacturer->components->count() }}
-            </small>
-            </td>
-
-          </tr>
-        @endforeach
-      </tbody>
-    </table>
-
-  </div> <!-- END OF PANEL -->
-    </div>
-
-  </div>
-
-
-
 </div> <!-- END OF CONTAINER -->
 
 @stop
